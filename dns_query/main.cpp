2d4548f32f501dee580c3c5fc962d3d266fcf2d9
“abhi” <abhichouhan@proton.me>
2024-07-03 23:54:03 +0530

    Initial Commit

---
+++ dns_query/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dns_query
+//
+//  Created by Abhishek Chouhan on 03/07/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
